141d559c8db845ead3d5469f43442b9f33672761
Santiago Aguirre <Santiaguirre10@users.noreply.github.com>
2019-08-23 22:23:47 -0300

    Creacion y moviento de los puppys

---
+++ Assets/Scripts/PuppyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppyController : MonoBehaviour
+{
+    GameObject escapepoint;
+    public float speed;
+    PuppyManager puppymanager;
+    GameManager gamemanager;
+    public int vida;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        gameObject.tag = "EnZona";
+        escapepoint = GameObject.Find("EscapePoint");
+        puppymanager = GameObject.FindObjectOfType<PuppyManager>();
+        gamemanager = GameObject.FindObjectOfType<GameManager>();
+    }
+    void Start()
+    {
+        gameObject.tag = "EnZona";
+        puppymanager.puppys.Add(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tag == "EnZona")
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else if (tag == "Escapando")
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, escapepoint.transform.position, step );
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Valla")
+        {
+            gameObject.tag = "Escapando";
+        }
+        if (collision.name == "EscapePoint")
+        {
+           gamemanager.gameover = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PuppyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppyManager : MonoBehaviour
+{
+    public GameObject puppy1;
+    public GameObject[] puppyspamer;
+    public List<GameObject> puppys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("PuppyCreator", 0, 5);
+        for (int i = 0; i < puppyspamer.Length; i++)
+        {
+           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PuppyCreator()
+    {
+        int rdm = Random.RandomRange(0 , 3);
+        Instantiate(puppy1, puppyspamer[rdm].transform.position , Quaternion.identity);
+    }
+}
