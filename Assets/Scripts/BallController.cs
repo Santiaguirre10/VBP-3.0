3843925b130fca96ff26ff3020d64e5c3e1a68ef
Santiago Aguirre <Santiaguirre10@users.noreply.github.com>
2019-12-04 15:58:43 -0300

    Pruebas curvas bezier

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -12,6 +12,7 @@ public class BallController : MonoBehaviour
     public Transform setpoint;
     float x;
     float y;
+    float count = 0.0f; 
 
     // Start is called before the first frame update  -0.67
     void Start()
@@ -21,7 +22,11 @@ public class BallController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {Move();
+    {
+    }
+    private void FixedUpdate()
+    {
+        Move();
     }
     void Move()
     {
@@ -37,8 +42,17 @@ public class BallController : MonoBehaviour
         }
         else if (tag == "Rebote")
         {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(x, y), step);
+            Vector3 pmax = transform.position + (new Vector3(x, y, 0f) - transform.position) / 2 + Vector3.up * 5.0f;
+            //float step = speed * Time.deltaTime;
+            //transform.position = Vector2.MoveTowards(transform.position, new Vector2(x, y), step);
+            if (count < 1.0f)
+            {
+                count += 0.1f * Time.deltaTime;
+
+                Vector3 m1 = Vector3.Lerp(transform.position, pmax, count);
+                Vector3 m2 = Vector3.Lerp(pmax, new Vector3(x, y, 0f), count);
+                transform.position = Vector3.Lerp(m1, m2, count);
+            }
         }
         else if (tag == "Defensa")
         {
@@ -47,8 +61,17 @@ public class BallController : MonoBehaviour
         }
         else if (tag == "Armado")
         {
-             float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, hitpoint, step);
+            Vector3 pmax = transform.position + (hitpoint - transform.position) / 2 + Vector3.up * 5.0f;
+            // float step = speed * Time.deltaTime;
+            //transform.position = Vector2.MoveTowards(transform.position, hitpoint, step);
+            if (count < 1.0f)
+            {
+                count += 0.5f * Time.deltaTime;
+
+                Vector3 m1 = Vector3.Lerp(transform.position, pmax, count);
+                Vector3 m2 = Vector3.Lerp(pmax, hitpoint, count);
+                transform.position = Vector3.Lerp(m1, m2, count);
+            }
         }
     }
     void OnTriggerEnter2D(Collider2D collision)
